ec780ca5b42ef1d5d24f5e0c238fd7a3baa8cc9b
Randy Martínez <randyma01@gmail.com>
2020-09-22 17:27:10 -0600

    div_t(x) completa

--- parte_i/div_t.m
+++ parte_i/div_t.m
@@ -1,18 +1,38 @@
-function [espn] = div_t(a)
-    espn = 0;    
-    if ((factorial(80) < a) &&  (a <= factorial(100)))
-        epsn = eps^15
-    elseif ((factorial(60) < a) &&  (a <= factorial(80)))
-        epsn = eps^11
-    elseif ((factorial(40) < a) &&  (a <= factorial(60)))
-        epsn = eps^8
-    elseif ((factorial(20) < a) &&  (a <= factorial(40)))
-        epsn = eps^4
-    elseif ((factorial(0) < a) &&  (a <= factorial(20)))
-        epsn = eps^2
+function x = div_t(a)
+    tol = 10^-8;
+    err = tol + 1;
+    k = 0;
+    x = 0.1;
+
+    if (factorial(0) < a && a <= factorial(20))
+        x = eps^2;
+        disp("primero")
+    elseif (factorial(20) < a && a <= factorial(40))
+         x = eps^4;
+        disp("segundo")
+    elseif (factorial(40) < a && a <= factorial(60))
+        x = eps^8;
+        disp("tercero")
+    elseif (factorial(60) < a && a <= factorial(80))
+        x = eps^11;
+        disp("cuarto")
+    elseif (factorial(80) < a && a <= factorial(100))
+        x = eps^15;
+        disp("quinto")
     end
-    espn2 = espn
+
+    while (tol <= err && k <= 2500)
+        disp("------------------")
+        disp("Iteracion numero: ")
+        k
+        disp("------------------")
+        xn = x * (2 - (a * x))
+        err = abs((xn - x)/xn)
+        x = xn
+        ++k
+    end
+    disp("******************")
 end
 
 
-% [espn] = div_t(2)
+% [x] = div_t(2)
